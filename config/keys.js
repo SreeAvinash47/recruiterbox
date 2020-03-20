6b28d8a16063b7a5cf003244dc4ed08cb910e737
unknown <sreeavinash.madiki@gmail.com>
2020-03-20 23:15:20 +0530

    Added Mongo Db

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://SreeAvinash47:Hitman47@@mernapp-wj5ac.mongodb.net/test?retryWrites=true&w=majority'
+}
--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
+const mongoURI = require('./config/keys').mongoURI;
 const app = express();
+
+// Connect to MongoDB
+
+mongoose
+  .connect(mongoURI, { useNewUrlParser: true,useUnifiedTopology: true }) // Let us remove that nasty deprecation warrning :)
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
 
 const port = process.env.PORT || 5000;
 
