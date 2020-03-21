1417eabc7517f770b07cd8f8b5e8a5f50dfe7640
unknown <sreeavinash.madiki@gmail.com>
2020-03-21 21:13:27 +0530

    Complete user setup - full length postman-check needed

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://SreeAvinash47:Hitman47@@mernapp-wj5ac.mongodb.net/test?retryWrites=true&w=majority'
+    mongoURI: 'mongodb+srv://SreeAvinash47:Hitman47@@mernapp-wj5ac.mongodb.net/test?retryWrites=true&w=majority',
+    secretOrKey: 'secret'
 }
--- server.js
+++ server.js
@@ -1,16 +1,36 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const mongoURI = require('./config/keys').mongoURI;
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const users = require('./routes/api/users');
+
 const app = express();
 
-// Connect to MongoDB
+//middlewares
 
+// bodyparser middleware
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(bodyParser.json());
+
+// Connect to MongoDB
 mongoose
-  .connect(mongoURI, { useNewUrlParser: true,useUnifiedTopology: true }) // Let us remove that nasty deprecation warrning :)
+  .connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }) // Let us remove that nasty deprecation warrning :)
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
+// Passport middleware
+app.use(passport.initialize());
 
+// Passport Config
+require('./config/passport')(passport);
+//routes
+app.use('/api/users', users);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, ()=>console.log(`Server listening on port: ${port}`))
+app.listen(port, () => console.log(`Server listening on port: ${port}`));
